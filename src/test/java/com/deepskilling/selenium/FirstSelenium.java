e36498980aafbfb1e9bf6e874b11706bb4257574
Administrator <swapnilgujar@hotmail.com>
2023-08-09 07:44:52 +0000

    modified scripts

--- src/test/java/com/deepskilling/selenium/FirstSelenium.java
+++ src/test/java/com/deepskilling/selenium/FirstSelenium.java
@@ -1,7 +1,9 @@
 package com.deepskilling.selenium;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class FirstSelenium {
 	
@@ -10,10 +12,19 @@ public class FirstSelenium {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://globalsqa.com/samplepagetest");
 		System.out.println(driver.getCurrentUrl());
-		System.out.println(driver.getTitle());
+		driver.findElement(By.id("g2599-name")).sendKeys("Name");
+		driver.findElement(By.id("g2599-email")).sendKeys("test123@test.com");
+		driver.findElement(By.id("g2599-website")).sendKeys("https://www.test.com");
 		System.out.println(driver.getTitle());
 		
-		driver.close();
+		Select dropDownValue = new Select(driver.findElement(By.id("g2599-experienceinyears")));
+		dropDownValue.selectByIndex(1);
+		
+		driver.findElement((By.xpath("//input[@type='checkbox' and @value='Functional Testing']"))).click();
+		driver.findElement((By.xpath("//input[@type='radio' and @value='Graduate']"))).click();
+		driver.findElement((By.id("contact-form-comment-g2599-comment"))).sendKeys("test comments");
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		//driver.close();
 	}
 
 }
--- src/test/java/com/deepskilling/selenium/SecondTestSelenium.java
+++ src/test/java/com/deepskilling/selenium/SecondTestSelenium.java
@@ -1,5 +1,6 @@
 package com.deepskilling.selenium;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -9,9 +10,12 @@ public static void main(String[] args) {
 		
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://the-internet.herokuapp.com/login");
-		System.out.println(driver.getCurrentUrl());
+		driver.findElement(By.id("username")).sendKeys("tomsmith");
+		driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
+		driver.findElement(By.xpath("//button[@class='radius']")).click();
+		//System.out.println(driver.getCurrentUrl());
 		System.out.println(driver.getTitle());
-		driver.close();
+		//driver.close();
 	}
 
 }
